21694147ff2193aea2070f3f87829d189e238a6a
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2025-07-10 19:15:45 +0700

    Create hari_tetap.py

---
+++ scripts/hari_tetap.py
@@ -0,0 +1,22 @@
+# Daftar hari libur nasional tetap per tahun
+# Format: {tahun: [(keterangan, tanggal), ...]}
+
+libur_tetap = {
+    2025: [
+        ("Tahun Baru Masehi", "1 Januari 2025"),
+        ("Isra Mikraj Nabi Muhammad SAW", "27 Februari 2025"),
+        ("Hari Raya Nyepi (Tahun Baru Saka 1947)", "29 Maret 2025"),
+        ("Wafat Yesus Kristus", "18 April 2025"),
+        ("Hari Buruh Internasional", "1 Mei 2025"),
+        ("Kenaikan Yesus Kristus", "29 Mei 2025"),
+        ("Hari Raya Idulfitri 1446 H", "30 April - 1 Mei 2025"),
+        ("Hari Raya Waisak 2569 BE", "12 Mei 2025"),
+        ("Hari Lahir Pancasila", "1 Juni 2025"),
+        ("Hari Raya Iduladha 1446 H", "6 Juni 2025"),
+        ("Tahun Baru Islam 1447 H", "26 Juni 2025"),
+        ("Hari Kemerdekaan Republik Indonesia", "17 Agustus 2025"),
+        ("Maulid Nabi Muhammad SAW", "5 September 2025"),
+        ("Hari Raya Natal", "25 Desember 2025"),
+    ],
+    # Tambahkan tahun lain di sini jika perlu
+}
